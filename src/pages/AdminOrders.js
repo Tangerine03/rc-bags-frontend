974c172e477ce0f6bc7b6e2c7fbb2d0fdd4d42d0
Tangerine03 <rcbags33@gmail.com>
2026-09-13 18:28:58 +0530

    Add missing useLocation import in TrackOrder

--- src/pages/AdminOrders.js
+++ src/pages/AdminOrders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { API_URL as BASE_URL } from '../config';
 import AdminNav from '../components/AdminNav';
 import { Link } from 'react-router-dom';
